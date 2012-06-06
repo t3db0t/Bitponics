5a2d4049b9f80c80fcbe88c3e0370abb17420238
Ezer Longinus <arimathaea@gmail.com>
2012-06-06 02:07:14 -0400

    new file:   hardware/arduino/WiFly_Test/Credentials.h

---
+++ hardware/arduino/WiFly_Test/Credentials.h
@@ -0,0 +1,9 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+// Wifi parameters
+char passphrase[] = "VibroMonk718";
+char ssid[] = "Vibromonk2";
+char ntp_server[] = "nist1-la.ustiming.org"; // or your favorite ntp server.
+
+#endif
